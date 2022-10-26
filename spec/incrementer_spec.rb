8322f922487e0f5cd268f59c1fb1d13b87e3a111
YEbaSHKA <drugon.vanya@bk.ru>
2022-10-27 01:19:33 +0300

    fix test

--- spec/incrementer_spec.rb
+++ spec/incrementer_spec.rb
@@ -5,7 +5,6 @@ require_relative '../incrementer'
 
 RSpec.describe Incrementer do
   context 'when given array' do
-    let(:value) { Incrementer.new([1, 2, 3]) }
-    it { expect(described_class(value).call).to eq([2, 4, 6]) }
+    it { expect(Incrementer.new([1, 2, 3]).call).to eq([2, 4, 6]) }
   end
 end
