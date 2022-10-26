d23eee5b0658e4c8d021d53118ad3d5cee3e153a
YEbaSHKA <drugon.vanya@bk.ru>
2022-10-26 22:44:09 +0300

    test fix

--- spec/incrementer_spec.rb
+++ spec/incrementer_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Incrementer do
     Incrementer.new([1, 2, 3])
   end
 
-  context 'given array' do
+  context 'when given array' do
     it { expect(incrementer.call).to eq([2, 4, 6]) }
   end
 end
