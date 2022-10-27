557c532c03938fc359a3c7458dbdff11d8bda801
YEbaSHKA <drugon.vanya@bk.ru>
2022-10-27 22:38:37 +0300

    disemvowel/fix

--- disemvowel.rb
+++ disemvowel.rb
@@ -2,11 +2,13 @@
 
 # class Dismvowel
 class Dismvowel
+  attr_reader :string
+
   def initialize(string)
     @string = string
   end
 
   def call
-    @string.delete('aeiouAEIOU')
+    string.delete('aeiouAEIOU')
   end
 end
